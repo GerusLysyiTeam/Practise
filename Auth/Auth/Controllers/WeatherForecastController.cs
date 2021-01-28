daf161b214f8a14558da86ea6401f43d50cf77cb
Oleh Gerus <ogeru@softserveinc.com>
2021-01-28 22:22:53 +0200

    replace api project - use another name

--- Auth/Auth/Controllers/WeatherForecastController.cs
+++ Auth/Auth/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace PractiseBackEnd.Controllers
+namespace Auth.Controllers
 {
     [ApiController]
     [Route("[controller]")]
